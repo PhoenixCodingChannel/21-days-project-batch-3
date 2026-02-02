b5b82a5d2693ecd8541ac5d4aa66c17bd943054f
vikasthakurr <vikaskumar20012001@gmail.com>
2026-02-02 22:21:51 +0530

    js pushed

---
+++ project-2/app.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.getElementById("themeToggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("light-theme") ? "light" : "dark",
+  );
+  const isLightTheme = document.body.classList.contains("light-theme");
+  toggleBtn.textContent = isLightTheme ? "🌙" : "🌞";
+});
